9164aa9ab8e45a9d3f1a33efc4fe5e45bb216097
dev <dev@VM-KUB-1.pjt.net>
2020-08-20 11:22:03 +0200

    first comit

---
+++ src/Controller/RootController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RootController extends AbstractController
+{
+    /**
+     * @Route("/", name="root")
+     */
+    public function index()
+    {
+
+
+        return $this->render('root/index.html.twig', [
+            'controller_name' => ' Maxime',
+        ]);
+
+    }
+}
